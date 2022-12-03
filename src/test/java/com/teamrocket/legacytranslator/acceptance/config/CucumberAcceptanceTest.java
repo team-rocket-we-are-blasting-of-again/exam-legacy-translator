3f7a49a6a6fbaa2624858bc3d53cda6fca9ea948
Tobias Zimmermann <tobias.zimmer007@gmail.com>
2022-12-03 23:29:53 +0100

    fix: use correct path in the cucumber config

--- src/test/java/com/teamrocket/legacytranslator/acceptance/config/CucumberAcceptanceTest.java
+++ src/test/java/com/teamrocket/legacytranslator/acceptance/config/CucumberAcceptanceTest.java
@@ -12,8 +12,8 @@ import org.junit.platform.suite.api.Suite;
 @ConfigurationParameter(key = Constants.PLUGIN_PROPERTY_NAME, value = "pretty")
 @ConfigurationParameter(
         key = Constants.GLUE_PROPERTY_NAME,
-        value = "com.teamrocket.Template.acceptance," +
-                "com.teamrocket.Template.acceptance.config"
+        value = "com.teamrocket.legacytranslator.acceptance," +
+                "com.teamrocket.legacytranslator.acceptance.config"
 )
 @ConfigurationParameter(key = Constants.PLUGIN_PUBLISH_QUIET_PROPERTY_NAME, value = "true")
 public class CucumberAcceptanceTest {
